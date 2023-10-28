80c88104e3ef05ce3ece62c095d6b4d08aebcf8d
DavideGila <davide.gila@gmail.com>
2023-10-28 16:25:36 +0200

    added image to createApp

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
 const { createApp } = Vue
-
 createApp({
     data() {
         return {
-            message: 'Titolo'
+            message: 'Goku',
+            image: "https://i.pinimg.com/736x/06/6a/e7/066ae79c961041fca3a1745a838f5f29.jpg"
         }
     }
 }).mount('#app')
+  
